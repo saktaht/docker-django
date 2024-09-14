2e28bea789f593d729ea899fb7cfe1e76bbda224
oyu <12345yuu6789@gmail.com>
2024-09-15 01:58:10 +0900

    バグの修正+コメント追加

--- src/mysite/accounts/models.py
+++ src/mysite/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
 
 class MyUserManager(BaseUserManager):
@@ -8,36 +8,45 @@ class MyUserManager(BaseUserManager):
             raise ValueError('Users must have an email address')
         if not password:
             raise ValueError('Users must have an password')
-
+        
+        # self.model は現在のユーザーモデルクラス（CustomUser）を参照
         user = self.model(
             email=self.normalize_email(email),
             password=password,
         )
+        # パスワードをハッシュ化 セキュリティの観点でも大切
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    # Djangoの createsuperuser コマンドを実行したとき使用される
     def create_superuser(self, email, password):
+        # まず通常のユーザーを作成
         user = self.create_user(
             email=self.normalize_email(email),
             password=password,
         )
+        # ユーザーに管理者権限を付与
         user.is_admin=True
+        # ユーザーにスタッフ権限を付与
         user.is_staff=True
         user.is_superuser=True
         user.save(using=self._db)
         return user
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name='Email', max_length=50, unique=True)
     password = models.CharField(max_length=128, unique=True, verbose_name='password')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    
     #AbstractBaseUserにはMyUserManagerが必要
     objects = MyUserManager()
     #一意の識別子として使用されます
     USERNAME_FIELD = 'email'
-    #ユーザーを作成するときにプロンプ​​トに表示されるフィールド名のリストです。
-    # REQUIRED_FIELDS = ['email']
 
+    # Django管理画面でユーザーリストを表示する際、この方法で返される文字列が各ユーザーの識別子として使用
     def __str__(self):
-        return self.username
+        return self.email
